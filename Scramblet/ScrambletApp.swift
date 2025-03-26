b75140008b1b18b06ad03336cac94afda9e9cd78
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-26 13:27:31 +0100

    Initial Commit

---
+++ Scramblet/ScrambletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambletApp.swift
+//  Scramblet
+//
+//  Created by Eric on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrambletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
